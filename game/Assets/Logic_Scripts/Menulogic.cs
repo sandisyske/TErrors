b793ba9e048f2c74bd8cd93824ea426bb9421511
kivipallur <102732123+kivipallur@users.noreply.github.com>
2024-04-13 15:26:57 +0300

    lil viga
    GAMEscene

--- game/Assets/Logic_Scripts/Menulogic.cs
+++ game/Assets/Logic_Scripts/Menulogic.cs
@@ -19,7 +19,7 @@ public class Menulogic : MonoBehaviour
     public void PlayNowButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
     }
 
     public void CreditsButton()
