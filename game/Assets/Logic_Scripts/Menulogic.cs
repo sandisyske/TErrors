2e25dc637058d2ed350092817b2ecd2dbb58cda7
kivipallur <102732123+kivipallur@users.noreply.github.com>
2024-04-13 13:28:41 +0300

    main menu mvp
    stx toores, aga töötab vist

---
+++ game/Assets/Logic_Scripts/Menulogic.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menulogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+        MainMenu.SetActive(true);
+    }
+
+    public void PlayNowButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
