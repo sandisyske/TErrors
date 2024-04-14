45f6c153f6c681bb310475de9bef5ce27c21e94e
Sandra <sandraerik9@gmail.com>
2024-04-14 14:10:39 +0300

    Relvast laskmine ja kuulid

--- game/Assets/Logic_Scripts/Projectile.cs
+++ game/Assets/Logic_Scripts/Projectile.cs
@@ -7,9 +7,11 @@ public class Projectile : MonoBehaviour
 {
 	public float speed;
 	public int damage;
+	public float timeToLive = 5f;
 	void Start()
 	{
-	}
+        Destroy(gameObject, timeToLive);
+    }
 	void Update()
 	{
 		transform.Translate(Vector3.up * speed * Time.deltaTime);
